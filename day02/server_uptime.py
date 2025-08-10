a44999d0e6eeb1aeca602f9753f0abd19d4c4d64
Pranav Kumar <pranav.das@hotmail.com>
2025-08-10 17:27:43 +0530

    fixed script

--- day02/server_uptime.py
+++ day02/server_uptime.py
@@ -1,16 +1,13 @@
 import os
 import subprocess
 
-# Get hostname (like `hostname` in bash)
-hostname = subprocess.run(["hostname"], capture_output=True, text=True).stdout.strip()
+hostname= subprocess.run(["hostname"] , capture_output=True, text==True).stdout.strip()
+uptime_server = subprocess.run(["uptime" , "-p"], capture_output=True, text=True).stdout.strip()
 
-# Get uptime output (like `uptime -p`)
-uptime_output = subprocess.run(["uptime", "-p"], capture_output=True, text=True).stdout.strip()
+with open("/proc/loadavg","r") as f:
+    load_avg = f.read().split()[0]
 
-# Get load average from /proc/loadavg
-with open("/proc/loadavg", "r") as f:
-    load_avg = f.read().split()[0]  # first number = 1-min load
+print(f"Hostname is {hostname}")
+print(f"Server is up from {uptime_server}")
+print(f"Load average is {load_avg}")
 
-print(f"Server: {hostname}")
-print(f"Uptime: {uptime_output}")
-print(f"Load Average (1 min): {load_avg}")
